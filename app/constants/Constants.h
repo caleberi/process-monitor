1f92f93c976b474c66d91361f2e7afc1680357a7
Caleb Adewole <caleberioluwa@gmail.com>
2021-07-23 15:45:17 +0100

    add constants and class Path for clean code reuse

---
+++ app/constants/Constants.h
@@ -0,0 +1,62 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <string>
+
+/**
+ * Defines the constants used by the process manager
+ * !\ DO NOT CHANGE THE ORDER OF THE CONSTANTS
+ */
+enum CPUstates
+{
+    S_USER = 0,
+    S_NICE,
+    S_SYSTEM,
+    S_IDLE,
+    S_IOWAIT,
+    S_IRQ,
+    S_SOFTIRQ,
+    S_STEAL,
+    S_GUEST,
+    S_GUEST_NICE,
+};
+
+class Path
+{
+public:
+    static std::string basePath()
+    {
+        return "/proc";
+    }
+    static std::string cmdPath()
+    {
+        return "/cmdline";
+    }
+
+    static std::string statusPath()
+    {
+        return "/status";
+    }
+
+    static std::string statPath()
+    {
+        return "stat";
+    }
+
+    static std::string upTimePath()
+    {
+        return "uptime";
+    }
+
+    static std::string memoInfoPath()
+    {
+        return "memoinfo";
+    }
+
+    static std::string versionPath()
+    {
+        return "version";
+    }
+}
+
+#endif /* CONSTANTS_H */
