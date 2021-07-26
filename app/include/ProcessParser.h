33281c1154e8614455f18f08fa99c3c6191ea1cb
Caleb Adewole <caleberioluwa@gmail.com>
2021-07-27 00:51:17 +0100

    add extra function implementation

--- app/include/ProcessParser.h
+++ app/include/ProcessParser.h
@@ -26,23 +26,20 @@ class ProcessParser
 {
 public:
     static std::string getCmd(std::string pid);
-    static std::vector<std::string> getCmds(std::string pid);
+    static std::vector<std::string> getPidList();
     static std::string getVmSize(std::string pid);
     static std::string getCpuPercent(std::string pid);
-    static long int getSysUptime();
-    static std::string getProcUptime(std::string pid);
+    static long int getSysUpTime();
+    static std::string getProcUpTime(std::string pid);
     static std::string getProcUser(std::string pid);
-    static std::vector<std::string> ProcessParser::getPidList();
-    static std::vector<std::string> getSysCpuPercents(std::string coreNumber = "");
+    static std::vector<std::string> getSysCpuPercent(std::string coreNumber = "");
     static float getSysRamPercent();
+    static int ProcessParser::getNumbersOfCores();
     static std::string getSysKernelVersion();
     static int getTotalThreads();
     static int getTotalNumberOfProcesses();
     static int getNumberOfRunningProcesses();
     static std::string getOsName();
-    static std::string getOsVersion();
-    static std::string getOsArchitecture();
-    static std::string getOsDistribution();
     static std::string printCpuStats(std::vector<std::string> value1, std::vector<std::string> value2);
 };
 
@@ -82,7 +79,7 @@ std::string ProcessParser::getCpuPercent(std::string pid)
     float sTime = std::stof(tokens[15]);
     float csTime = std::stof(tokens[16]);
     float startTime = std::stof(tokens[21]);
-    float upTime = ProcessParser::getSysUptime();
+    float upTime = ProcessParser::getSysUpTime();
     float frequency = sysconf(_SC_CLK_TCK);
     float totalTime = upTime + sTime + csTime + upTime;
     float seconds = upTime - (startTime / frequency);
@@ -90,7 +87,7 @@ std::string ProcessParser::getCpuPercent(std::string pid)
     return std::to_string(result);
 };
 
-std::string ProcessParser::getProcUptime(std::string pid)
+std::string ProcessParser::getProcUpTime(std::string pid)
 {
     std::string line;
     std::ifstream stream;
@@ -103,7 +100,7 @@ std::string ProcessParser::getProcUptime(std::string pid)
     return std::to_string(std::stof(tokens[13]) / sysconf(_SC_CLK_TCK));
 }
 
-long int ProcessParser::getSysUptime()
+long int ProcessParser::getSysUpTime()
 {
     std::string line;
     std::string path{Path::basePath() + Path::upTimePath()};
@@ -176,4 +173,72 @@ std::string ProcessParser::getCmd(std::string pid)
     return line;
 };
 
+int ProcessParser::getNumbersOfCores()
+{
+    std::string line;
+    std::string searchParam = "cpu cores";
+    std::ifstream stream;
+    std::string path{Path::basePath() + "cpuinfo"};
+    float dataSize;
+    stream.open(path);
+    while (std::getline(stream, line))
+    {
+        if (line.compare(0, searchParam.size(), searchParam) == 0)
+        {
+            std::istringstream buffer(line);
+            std::istream_iterator<std::string> beg(buffer), end;
+            std::vector<std::string> tokens{beg, end};
+            return stoi(tokens[3]);
+        }
+    }
+    return 0;
+};
+
+std::vector<std::string> ProcessParser::getSysCpuPercent(std::string coreNumber)
+{
+    std::string line;
+    std::string name = "cpu" + coreNumber;
+    std::ifstream stream = Helpers::getStream((Path::basePath() + Path::statPath()));
+    while (std::getline(stream, line))
+    {
+        if (line.compare(0, name.size(), name) == 0)
+        {
+            std::istringstream buf(line);
+            std::istream_iterator<std::string> beg(buf), end;
+            std::vector<std::string> values(beg, end);
+            return values;
+        }
+    }
+    return (std::vector<std::string>());
+};
+
+std::string ProcessParser::printCpuStats(std::vector<std::string> values1, std::vector<std::string> values2)
+{
+    /*
+Because CPU stats can be calculated only if you take measures in two different time,
+this function has two parameters: two vectors of relevant values.
+We use a formula to calculate overall activity of processor.
+*/
+    float activeTime = getSysActiveCpuTime(values2) - getSysActiveCpuTime(values1);
+    float idleTime = getSysIdleCpuTime(values2) - getSysIdleCpuTime(values1);
+    float totalTime = activeTime + idleTime;
+    float result = 100.0 * (activeTime / totalTime);
+};
+
+float getSysActiveCpuTime(std::vector<std::string> values)
+{
+    return (std::stof(values[CPUstates::S_USER]) +
+            std::stof(values[CPUstates::S_NICE]) +
+            std::stof(values[CPUstates::S_SYSTEM]) +
+            std::stof(values[CPUstates::S_IRQ]) +
+            std::stof(values[CPUstates::S_SOFTIRQ]) +
+            std::stof(values[CPUstates::S_STEAL]) +
+            std::stof(values[CPUstates::S_GUEST]) +
+            std::stof(values[CPUstates::S_GUEST_NICE]));
+}
+
+float getSysIdleCpuTime(std::vector<std::string> values)
+{
+    return (stof(values[CPUstates::S_IDLE]) + stof(values[CPUstates::S_IOWAIT]));
+}
 #endif // PROCESSPARSER_H
